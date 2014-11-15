adaf82a504541b467a5dc57910b717c9acb42d5a
tatsujinichi <tatsujinichi@gmail.com>
2014-11-16 03:38:11 +0900

    added missing files

---
+++ Tests/ModelParser/BinaryHelperTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelParser;
+using NUnit.Framework;
+
+namespace Tests.ModelParser
+{
+	[TestFixture]
+	class BinaryHelperTests
+	{
+		[Test]
+		public void ToObject()
+		{
+			var testData = new TestData
+			{
+				Name = "john",
+				Ints = new List<int> {1, 2, 3},
+				NestedTestData = new TestData
+				{
+					Name = "nestedName",
+					Ints = new List<int> {4, 5, 6},
+					NestedTestData = new TestData()
+				}
+			};
+			var bytes = testData.ToBytes();
+			var obj = bytes.ToObject<TestData>();
+			Assert.AreEqual(testData.Name, obj.Name);
+			Assert.True(testData.Ints.SequenceEqual(obj.Ints));
+			Assert.AreEqual(testData.NestedTestData.Name, obj.NestedTestData.Name);
+			Assert.True(testData.NestedTestData.Ints.SequenceEqual(obj.NestedTestData.Ints));
+		}
+
+		[Test]
+		public void ToBytes()
+		{
+			var testData = new TestData
+			{
+				Name = "john",
+				Ints = new List<int> { 1, 2, 3 },
+				NestedTestData = new TestData
+				{
+					Name = "nestedName",
+					Ints = new List<int> { 4, 5, 6 },
+					NestedTestData = new TestData()
+				}
+			};
+			var testData2 = new TestData
+			{
+				Name = "john",
+				Ints = new List<int> { 1, 2, 3 },
+				NestedTestData = new TestData
+				{
+					Name = "nestedName",
+					Ints = new List<int> { 4, 5, 6 },
+					NestedTestData = new TestData()
+				}
+			};
+			var bytes1 = testData.ToBytes();
+			var bytes2 = testData2.ToBytes();
+
+			Assert.True(bytes1.SequenceEqual(bytes2));
+		}
+
+		[Serializable]
+		private class TestData
+		{
+			public string Name { get; set; }
+			public List<int> Ints { get; set; }
+			public TestData NestedTestData { get; set; }
+		}
+	}
+}
---
+++ Tests/ModelParser/XmlHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Collada141;
+using ModelParser;
+using NUnit.Framework;
+
+
+namespace Tests
+{
+	[TestFixture]
+	public class XmlHelperTests
+	{
+		[Test]
+		public void Load()
+		{
+			var testmodel = XmlHelper.Load<COLLADA>(@"C:\dev\Senjin\ColladaReader\res\untitled.dae");
+			Assert.NotNull(testmodel);
+		}
+
+		[Test]
+		public void Save()
+		{
+			var testmodel = XmlHelper.Load<COLLADA>(@"C:\dev\Senjin\ColladaReader\res\untitled.dae");
+			Assert.NotNull(testmodel);
+
+			XmlHelper.Save(@"C:\dev\Senjin\ColladaReader\res\untitled2.dae", testmodel);
+
+			var testmodel2 = XmlHelper.Load<COLLADA>(@"C:\dev\Senjin\ColladaReader\res\untitled2.dae");
+
+			//TODO: add more
+			var f1 = (library_cameras)testmodel.Items.First();
+			var f2 = (library_cameras)testmodel2.Items.First();
+			Assert.AreEqual(f1.id, f2.id);
+		}
+	}
+}
